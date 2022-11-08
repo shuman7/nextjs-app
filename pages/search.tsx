9bf4d69f0b43d07c01219bcdfe7ac04a37a2b205
shuman7 <shuman1231@gmail.com>
2022-11-08 17:40:08 +0900

    add search result / search box

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch } from 'react-instantsearch-hooks-web';
+import { Hits, HitsProps, InstantSearch, SearchBox } from 'react-instantsearch-hooks-web';
+import { Post } from '../types/posts';
 
-const searchClient = algoliasearch('0IAEHW2DF5', 'fb467724389477e30d3f2cc415ffa826');
+const searchClient = algoliasearch(
+    '0IAEHW2DF5', 
+    'fb467724389477e30d3f2cc415ffa826'
+);
+
+const Hit: HitsProps<Post>['hitComponent'] = ({hit}) => {
+    return <div>{hit.title}</div>
+}
 
 const Search = () => {
   return (
     <div>
         <h1>検索</h1>
+
+        <InstantSearch indexName="posts" searchClient={searchClient}>
+            <SearchBox /> 
+            <Hits<Post> hitComponent={Hit} />
+        </InstantSearch>
     </div>
   )
 }
