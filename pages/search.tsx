2425e027132f5039e3200bfe29ce84a0f7d54b37
shuman7 <shuman1231@gmail.com>
2022-11-08 18:06:00 +0900

    add no results

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import algoliasearch from 'algoliasearch/lite';
-import { Hits, HitsProps, InstantSearch, SearchBox } from 'react-instantsearch-hooks-web';
+import { Hits, HitsProps, InstantSearch, SearchBox, SearchBoxProps, useInstantSearch } from 'react-instantsearch-hooks-web';
 import { Post } from '../types/posts';
+import { debounce } from 'debounce';
 
 const searchClient = algoliasearch(
     '0IAEHW2DF5', 
@@ -12,14 +13,30 @@ const Hit: HitsProps<Post>['hitComponent'] = ({hit}) => {
     return <div>{hit.title}</div>
 }
 
+const NoResultsBoundary = ({children}: {children: ReactNode}) => {
+    const { results } = useInstantSearch();
+
+    if(!results.__isArtificial && results.nbHits === 0) {
+        return <p>「{results.query}」検索結果はありませんでした</p>
+    } 
+
+    return <>{children}</>
+}
+
 const Search = () => {
+    const search: SearchBoxProps['queryHook'] = (query, hook) => {
+        hook(query)
+    }
+
   return (
     <div>
         <h1>検索</h1>
 
         <InstantSearch indexName="posts" searchClient={searchClient}>
-            <SearchBox /> 
-            <Hits<Post> hitComponent={Hit} />
+            <SearchBox queryHook={debounce(search, 500)} />
+            <NoResultsBoundary>
+                <Hits<Post> hitComponent={Hit} />
+            </NoResultsBoundary>
         </InstantSearch>
     </div>
   )
