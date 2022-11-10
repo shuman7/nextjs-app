41cb366f8ffa40c8d6e55c6ff69c573de100a974
shuman7 <shuman1231@gmail.com>
2022-11-10 16:23:33 +0900

    add getStaticPaths, getStaticProps

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,36 +1,62 @@
 import { format } from 'date-fns';
 import { doc, getDoc } from 'firebase/firestore';
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
+import Link from 'next/link';
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import useSWR from 'swr/immutable';
 import { db } from '../../firebase/client';
+import { adminDB } from '../../firebase/server';
 import { useUser } from '../../lib/user';
 import { Post } from '../../types/posts';
 import { User } from '../../types/user';
 
-const PostDetailPage = () => {
-    const [post, setPost] = useState<Post>();
-    const router = useRouter();
-    const postId = router.query.id;
+
+/////////////////////////////////////// 
+////////// ↓next.jsにおけるnode.jsの領域↓
+// ストックを作るためにfirabaseからサーバーに持ってくる作業をするのが getStaticProps
+export const getStaticProps: GetStaticProps<{
+        post: Post;
+    }> = async (context) => {
+    const snap = await adminDB.doc(`posts/${context.params?.id}`).get() //このidはファイル名の[]内のテキストと同じ。[post].tsxであれば、ここはpostになる。
+    const post = snap.data() as Post
+
+    return {
+        props: {
+            post,
+            // post: post これと上記はjs的には全く一緒
+        },
+    }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    // getStaticPathsはあらかじめ記事のidがわかっているものについては予め作るから教えてよという機能
+    return {
+        paths: [],
+        //記事を生成している時のロード画面見せるか見せないかの設定 
+        // やる場合はtrue / やらない場合はfalse / blokingは生成中何も表示させない。生成されたらページを表示する。実装コストが少ない。
+        fallback: 'blocking', 
+    }
+}
+////////// ↑next.jsにおけるnode.jsの領域↑
+///////////////////////////////////////
+
+const PostDetailPage = ({
+    post,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
     const user = useUser(post?.authorId);
 
-    useEffect(() => {
-        const ref = doc((db), `posts/${postId}`)
-        getDoc(ref).then(snap => {
-            setPost(snap.data() as Post)
-        })
-    }, [postId])
-
-    useEffect(() => {
-
-    }, [post])
-
     if(!post) {
-        return null;
+        return <p>記事が存在しません</p>;
     }
 
   return (
       <div className="container">
+          <p>
+              <Link href="/search">
+                <a>Search</a>
+              </Link>
+          </p>
           <div className="aspect-video bg-slate-200 mb-4 rounded-md"></div>
           <h1 className="font-bold text-lg mb-6">{post.title}</h1>
           {user && (
