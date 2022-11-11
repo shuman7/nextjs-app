f1e3ad501fe5460decd1a9deec3f1f38803c0408
shuman7 <shuman1231@gmail.com>
2022-11-11 16:44:02 +0900

    edit sidebar layout

--- components/header.tsx
+++ components/header.tsx
@@ -1,31 +1,51 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '../context/auth'
+import Sidebar from './sidebar'
 import UserMenu from './user-menu'
+import { Bars3Icon } from '@heroicons/react/24/outline'
 
 const Header = () => {
   const {user, isLoading} = useAuth();
-
-
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false)
   if (isLoading) {
     return null;
   }
+
+  const closeModal = () => {
+    setIsSidebarOpen(false);
+  }
+  const openModal = () => {
+    setIsSidebarOpen(true);
+  }
+
   return (
-    <header>
+    <>
+      <header>
           <div className="container">
             <div className="flex items-center h-14 border-b container">
+              <button type='button' className="p-2 mr-1" onClick={openModal}>
+                <Bars3Icon className="w-10 h-10" />
+              </button>
               <Link href="/">
                 <a className='flex'>
                   <Image src="/logo.svg" width={105} height={40} alt="Logoipsum" />
                 </a>
               </Link>
               <span className="flex-1" />
-              {user ? (<UserMenu />) : (<Link href="/login"><a>ログイン</a></Link>)}
+              {user ? (<UserMenu />
+              ) : (
+                <Link href="/login">
+                  <a>ログイン</a>
+                </Link>
+              )}
             </div>
           </div>
         </header>
+        <Sidebar isOpen={isSidebarOpen} closeModal={closeModal}/>
+      </>
   )
 }
 
-export default Header
+export default Header;
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,16 +1,57 @@
 import { Dialog, Transition } from '@headlessui/react'
+import Image from 'next/image';
+import Link from 'next/link';
 import { Fragment, useState } from 'react'
 
-const Sidebar = ({isOpen, closeModal}: { isOpen: boolean, closeModal: VoidFunction}) => {
-  let [isOpen, setIsOpen] = useState(true)
+const links = [
+  {
+    label: 'ホーム',
+    href: '/'
+  },
+  {
+    label: '検索',
+    href: '/search'
+  },
+  {
+    label: 'このサイトについて',
+    href: '/about'
+  },
+]
 
-  function closeModal() {
-    setIsOpen(false)
-  }
+const subItems = [
+  {
+    label: '会社概要',
+    href: '/company'
+  },
+  {
+    label: 'プライバシーポリシー',
+    href: '/privacy'
+  },
+  {
+    label: '利用規約',
+    href: '/terms'
+  },
+  {
+    label: 'サポート',
+    href: '/support'
+  },
+  {
+    label: 'お問合せ',
+    href: '/contact'
+  },
+  {
+    label: 'ヘルプ',
+    href: '/help'
+  },
+]
 
-  function openModal() {
-    setIsOpen(true)
-  }
+const Sidebar = ({
+  isOpen, 
+  closeModal,
+}: { 
+  isOpen: boolean;
+   closeModal: VoidFunction;
+  }) => {
 
   return (
     <>
@@ -31,19 +72,41 @@ const Sidebar = ({isOpen, closeModal}: { isOpen: boolean, closeModal: VoidFuncti
           <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
+                enterFrom="opacity-0 -translate-x-full"
+                enterTo="opacity-100 scale-100 translate-x-0"
                 leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
+                leaveFrom="opacity-100 translate-x-0"
+                leaveTo="opacity-0 -translate-x-full"
               >
-                <Dialog.Panel className="w-80 fixed left-0 inset-y-0 bg-white p-10 z-20">
-                    <ul>
-                        <li>メニュー</li>
-                        <li>メニュー</li>
-                        <li>メニュー</li>
-                        <li>メニュー</li>
+                <Dialog.Panel className="w-80 fixed left-0 inset-y-0 bg-white p-6 z-20 overflow-y-auto">
+                  <Link href="/">
+                  <a className='flex mb-6'>
+                    <Image src="/logo.svg" width={105} height={40} alt="Logoipsum" />
+                  </a>
+                  </Link>
+                    <ul className="space-y-3">
+                      {links.map((item) => (
+                        <li key={item.label}>
+                          <Link href={item.href}>
+                            <a className="py-1 block hover:text-blue-500">
+                              {item.label}
+                            </a>
+                          </Link>
+                        </li>
+                      ))}
                     </ul>
+                    <hr className="my-6"/>
+                    <ul className="space-y-2">
+                      {subItems.map((item) => (
+                        <li key={item.label}>
+                          <Link href={item.href}>
+                            <a className="py-1 block hover:text-blue-500 text-slate-600">{item.label}</a>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+
+                    <p className="mt-6 text-slate-400">© seno production.</p>
                 </Dialog.Panel>
               </Transition.Child>
         </Dialog>
