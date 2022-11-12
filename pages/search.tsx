19a959ebae53699d4aea41f3c39bf2f7548db8fa
shuman7 <shuman1231@gmail.com>
2022-11-12 18:38:01 +0900

    search page add profile image

--- pages/search.tsx
+++ pages/search.tsx
@@ -38,10 +38,17 @@ const Hit: HitsProps<Post>['hitComponent'] = ({hit}) => {
                     <a>{hit.title}</a>
                 </Link>
             </h2>
-            <p className='text-slate-500'>
-                {format(hit.createdAt, 'yyyy年MM月dd日')}
-            </p>
-            {user && <p className="truncate">{user.name}</p>}
+            {user && (
+                <div className="flex items-center">
+                    <img src={user?.avatarURL} alt="" className='w-10 h-10 block rounded-full mr-2' />
+                    <div>
+                        <p className="truncate">{user.name}</p>
+                        <p className="text-slate-500 text-sm">
+                            {format(hit.createdAt, 'yyyy年MM月dd日')}
+                        </p>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
